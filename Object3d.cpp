4178cac515831a6ce38d4e6c6e782df1261a7ce3
kobashi_hayato <k021g1128@g.neec.ac.jp>
2022-10-21 14:02:50 +0900

    2-1_テクスチャと法線

--- Object3d.cpp
+++ Object3d.cpp
@@ -404,7 +404,7 @@ void Object3d::CreateModel()
 	//ファイルストリーム
 	std::ifstream file;
 	//.objファイルを開く
-	file.open("Resources/triangle/triangle.obj");
+	file.open("Resources/triangle_tex.obj");
 	//ファイルオープン失敗をチェック
 	if (file.fail()) {
 		assert(0);
@@ -435,11 +435,33 @@ void Object3d::CreateModel()
 			//座標データに追加
 			positions.emplace_back(position);
 			//頂点データに追加
-			VertexPosNormalUv vertex{};
+
+			/*VertexPosNormalUv vertex{};
 			vertex.pos = position;
-			vertices.emplace_back(vertex);
+			vertices.emplace_back(vertex);*/
 		}
 
+		if (key == "vt") {
+			//U,V成分読み込み
+			XMFLOAT2 texcoord{};
+			line_stream >> texcoord.x;
+			line_stream >> texcoord.y;
+			//V方向反転
+			texcoord.y = 1.0f - texcoord.y;
+			//テクスチャ座標データに追加
+			texcoords.emplace_back(texcoord);
+		}
+
+		//先頭文字列がvnなら法線ベクトル
+		if (key == "vn") {
+			//X,Y,Z成分読み込み
+			XMFLOAT3 normal{};
+			line_stream >> normal.x;
+			line_stream >> normal.y;
+			line_stream >> normal.z;
+			//法線ベクトルデータに追加
+			normals.emplace_back(normal);
+		}
 		//先頭文字列がfならポリゴン
 		if (key == "f")
 		{
@@ -449,10 +471,21 @@ void Object3d::CreateModel()
 			{
 				//頂点インデックス1個分の文字数をストリームに変換して解析しやすくする
 				std::istringstream index_stream(index_string);
-				unsigned short indexPosition;
+				unsigned short indexPosition, indexNormal, indexTexcoord;
 				index_stream >> indexPosition;
+				index_stream.seekg(1, ios_base::cur);//スラッシュとばす
+				index_stream >> indexTexcoord;
+				index_stream.seekg(1, ios_base::cur);//スラッシュとばす
+				index_stream >> indexNormal;
+
+				//頂点データの追加
+				VertexPosNormalUv vertex{};
+				vertex.pos = positions[indexPosition - 1];
+				vertex.normal = normals[indexNormal - 1];
+				vertex.uv = texcoords[indexTexcoord - 1];
+				vertices.emplace_back(vertex);
 				//頂点インデックスに追加
-				indices.emplace_back(indexPosition - 1);
+				indices.emplace_back((unsigned short)indices.size());
 			}
 		}
 
